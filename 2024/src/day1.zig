5da4144452d1ebe6d88cc9b3355336dded3aaefa
Uy Ha <hchanuy@gmail.com>
2024-12-01 14:28:18 +0000

    day 1 - part 1

--- 2024/src/day1.zig
+++ 2024/src/day1.zig
@@ -7,8 +7,25 @@ fn openFile(path: []const u8, flags: std.fs.File.OpenFlags) std.fs.File.OpenErro
     return std.fs.cwd().openFile(path, flags);
 }
 
+fn partOne(first_col: []i64, second_col: []i64) u64 {
+    var result: u64 = 0;
+
+    std.mem.sort(i64, first_col, {}, comptime std.sort.asc(i64));
+    std.mem.sort(i64, second_col, {}, comptime std.sort.asc(i64));
+
+    for (first_col, second_col) |f, s| {
+        result += @abs(f - s);
+    }
+
+    return result;
+}
+
 pub fn main() !void {
-    const allocator = std.heap.page_allocator;
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.detectLeaks();
+
+    const allocator = gpa.allocator();
+
     const args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args);
 
@@ -21,6 +38,25 @@ pub fn main() !void {
 
     const file_stat = try file.stat();
     const content = try file.readToEndAlloc(allocator, file_stat.size);
+    defer allocator.free(content);
 
-    std.debug.print("{s}", .{content});
+    var first_col = std.ArrayList(i64).init(allocator);
+    defer first_col.deinit();
+
+    var second_col = std.ArrayList(i64).init(allocator);
+    defer second_col.deinit();
+
+    var split = std.mem.split(u8, content, "\n");
+    while (split.next()) |line| {
+        if (line.len == 0) continue;
+
+        var columns = std.mem.split(u8, line, "   ");
+        const first = try std.fmt.parseInt(i64, columns.next().?, 10);
+        const second = try std.fmt.parseInt(i64, columns.next().?, 10);
+
+        try first_col.append(first);
+        try second_col.append(second);
+    }
+
+    std.debug.print("{}\n", .{partOne(first_col.items, second_col.items)});
 }
